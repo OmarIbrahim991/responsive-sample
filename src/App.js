528f6f97c11f88fef9cbe8acafac17193e4d2779
Omar Ibrahim <omar.ibrahim.991@gmail.com>
2021-10-25 01:26:57 +0200

    Add table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useLayoutEffect, useState } from 'react'
+import { get } from './utils/api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useWindowWidth = () => {
+	const [width, setWidth] = useState(null)
+
+	useLayoutEffect(() => {
+		const updateWidth = () => {
+			setWidth(window.innerWidth)
+		}
+		window.addEventListener('resize', updateWidth)
+		updateWidth()
+		return () => window.removeEventListener('resize', updateWidth)
+	}, [])
+	return width <= 600 ? "list" : "table"
 }
 
-export default App;
+const App = () => {
+	const view = useWindowWidth()
+	const [products, setProducts] = useState([])
+
+	const loadProduct = (id) => {
+		get({ id }).then(product => setProducts(current => current.map(p => p.id === product.id ? product : p)))
+	}
+
+	useEffect(() => {
+		get({ view }).then(data => setProducts(data))
+	}, [view])
+
+	if (!view || products.length === 0) return <h1>Loading...</h1>
+
+	if (view === "list") {
+		return (
+			<main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+				{products.map(({ id, title, description, price, category, image }) => (
+					<details key={id} onClick={() => loadProduct(id)}>
+						<summary>{id}</summary>
+						{
+							title ?
+								<ul>
+									<li>{title}</li>
+									<li>{price}</li>
+									<li>{description}</li>
+									<li>{category}</li>
+									<li>{image}</li>
+								</ul>
+							:
+								<p>Loading...</p>
+						}
+					</details>
+			))}
+			</main>
+		)
+	}
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					{Object.keys(products[0]).map(header => <td key={header}>{header}</td>)}
+				</tr>
+			</thead>
+			<tbody>
+				{
+					products.map(({ id, title, price, description, category, image }) => (
+						<tr key={id}>
+							<td>{id}</td>
+							<td>{title}</td>
+							<td>{price}</td>
+							<td>{description}</td>
+							<td>{category}</td>
+							<td>{image}</td>
+						</tr>
+					))
+				}
+			</tbody>
+	</table>
+	)
+}
+
+export default App
